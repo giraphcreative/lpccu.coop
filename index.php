7cb3e0e72c015e5ac05aef3f9ce09cc23c069925
James Pederson <james@jpederson.com>
2018-06-15 14:49:25 -0500

    Update search results title output so that it strips tags before encoding special characters.

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ get_header(); ?>
 
 		<?php
 		if ( is_search() ) {
-			?><h1 class="post-title">Search Results for <span>'<?php print htmlspecialchars( $_REQUEST["s"] ); ?>'</span></h1><?php
+			?><h1 class="post-title">Search Results for <span>'<?php print htmlspecialchars( striptags( $_REQUEST["s"] ) ); ?>'</span></h1><?php
 		} else {
 			?><h1 class="post-title">Our Blog</h1><?php
 		}
